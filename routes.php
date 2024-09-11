514628a08f6f3969e8b05a7c91730e2462c61db1
ashuro-ryuki <lffxds@gmail.com>
2024-09-11 09:48:40 -0300

    puxa os arquivos entre si

---
+++ routes.php
@@ -0,0 +1,12 @@
+<?php 
+
+// inclui arquivo de controlador para lidar com diferentes acoes
+
+require 'controllers/AuthController.php';
+// inclui controlador de autenticacao
+require 'controllers/DashboardController.php';
+// inclui controlador de usurio
+require 'controllers/UseController.php';
+// inclui controlador do dashboard
+
+?>
