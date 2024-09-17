d7686c69df07d06a82b6e172c95769c517a16835
ashuro-ryuki <lffxds@gmail.com>
2024-09-17 12:09:24 -0300

    cria os campos de preenchimento de login  e cadastro

---
+++ views/register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar-se</title>
+</head>
+<body>
+   <div>
+    <h2>Cadastro de Usuário</h2>
+ <form action="" method="post">
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" id="nome" required>
+    <label for="email">E-mail</label>
+    <input type="email" name="email" id="email" required>
+     <label for="email">E-mail</label>
+    <input type="email" name="email" id="email" required>
+    <label for="senha">Senha</label>
+    <input type="password" name="senha" id="senha" required>
+ <label for="perfil">Perfil</label>
+ <select name="perfil" id="perfil">
+  <option value="admin">Admin</option>
+  <option value="gestor">Gestor</option>
+  <option value="colaborador">Colaborador</option>
+  </select>
+
+  <button type="submit">Cadastrar</button>
+</form>
+<a href="">Voltar ao login</a>
+   </div> 
+</body>
+</html>
