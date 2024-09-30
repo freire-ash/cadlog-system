2aa7fdce872d45ee8bbaf7a027d88599c92e5c40
ashuro-ryuki <lffxds@gmail.com>
2024-09-30 07:36:24 -0300

    teste

--- database.sql
+++ database.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE sistema_usuarios
+Use sistema_usuarios;
+
+CREATE TABLE  usuarios(
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    nome VARCHAR (100) NOT NULL,
+    email  VARCHAR (100) NOT NULL UNIQUE,
+    senha VARCHAR (255) NOT NULL,
+    perfil ENUM('admin,''gestor','colaborador') NOT NULL, created_at TIMESTAMP DEFALT CURRENT_TIMESTAMP);
+    
--- models/database.php
+++ models/database.php
@@ -1,6 +1,25 @@
 <?php 
 class Database{
     //ultiliza padrao singletom vujo o objetivo e garantir qur apenas uma nunica instancia de classse seja criada durante a execução do programa, e que essa instancia seja ultilizada sempre necessario
+
+private static $instance = null;
+
+//metodo publico que retorna a conexão com BD
+
+public static function getConnection(){
+
+if(!self::$instance){
+    $host          = 'localhost';
+    $db            = 'sistema_usuarios';
+    $user          = 'root';
+    $password      = '';
+    // a conexão usa driver Mysql (mysql:) as informções de host e DB
+self::$instance = new PDO("mysql:host=$host;dbname=$db",$user, $password);
+//define o medo de erro para exceções, facilitando a depuração e tratamento de erro 
+self::$instance->setAttribute(PDO ::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}
+}
 }
 
 
